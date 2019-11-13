f15a7eff84f0f2b16ee1ddaecedb9effc6f9ffc0
Amy Balmforth <amy.balmforth@gmail.com>
2019-11-13 15:38:08 +0000

    has powersaving mode and max temp

--- spec/thermoSpec.js
+++ spec/thermoSpec.js
@@ -12,20 +12,29 @@ describe('Thermo', function() {
     expect(thermo.MIN_DEGREES).toEqual(10);
   });
 
-  it('up function raises temp by 10 degrees', function() {
+  it('up function raises temp by 5 degrees', function() {
     thermo.up();
-    expect(thermo.degrees).toEqual(30);
+    expect(thermo.degrees).toEqual(25);
   });
 
-  it('down function lowers temp by 10 degrees', function() {
+  it('down function lowers temp by 5 degrees', function() {
     thermo.down();
-    expect(thermo.degrees).toEqual(10);
+    expect(thermo.degrees).toEqual(15);
   });
 
   it('will not allow temp of lower than 10 degrees', function() {
     thermo.down();
     thermo.down();
+    thermo.down();
     expect(thermo.degrees).toEqual(10);
   });
+
+  it('power saving mode is on by default', function() {
+    expect(thermo.powerSaving).toEqual(true);
+  });
+
+  it('has max degrees of 25 when powersaving on', function() {
+    expect(thermo.MAX_DEGREES).toEqual(25);
+  })
 
 });
--- src/thermo.js
+++ src/thermo.js
@@ -1,15 +1,17 @@
 function Thermo() {
   this.degrees = 20;
   this.MIN_DEGREES = 10;
+  this.MAX_DEGREES = 25;
+  this.powerSaving = true;
 };
 Thermo.prototype.up = function() {
-  thermo.degrees += 10;
+  thermo.degrees += 5;
 };
 Thermo.prototype.down = function() {
   if (thermo.degrees === thermo.MIN_DEGREES) {
     thermo.degrees = 10;
   }
   else {
-    thermo.degrees -= 10;
+    thermo.degrees -= 5;
   }
 };
